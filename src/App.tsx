dfa8b4e7dfe8ea9e82cc01558cf79bc5e904d61e
Anonyme <xc891234@qq.com>
2024-10-18 16:02:09 +0800

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,6 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
